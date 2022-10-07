587b8ae7bf8b5e3982b613cedbd16424cbd23296
Stanisław Kumor <stanislav.kumor@gmail.com>
2022-10-07 10:30:51 +0200

    Add new event feature

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 import currentAcademicSemester
 from google_apis import create_service
 
@@ -10,7 +8,38 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 service = create_service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-request_body = {
+request_body_of_new_calendar = {
     'summary': currentAcademicSemester.current_semester
 }
-response = service.calendars().insert(body=request_body).execute()
+calendar_response = service.calendars().insert(body=request_body_of_new_calendar).execute()
+
+name = 'Techn.i systemy multimedialne'
+location = '1.17 CEUE'
+
+event = {
+    'summary': name,
+    'location': location,
+    'start': {
+        'dateTime': '2022-10-08T12:00:00',
+        'timeZone': 'Europe/Warsaw',
+    },
+    'end': {
+        'dateTime': '2022-10-08T13:00:00',
+        'timeZone': 'Europe/Warsaw',
+    },
+    'recurrence': [
+        'RRULE:FREQ=WEEKLY;UNTIL=20221030T170000Z' #yyyymmdd
+    ],
+
+    # 'reminders': {
+    #    'useDefault': False,
+    #    'overrides': [
+    #        {'method': 'email', 'minutes': 24 * 60},
+    #        {'method': 'popup', 'minutes': 10},
+    #    ],
+    # },
+}
+
+event_response = service.events().insert(
+    calendarId='29943d99a2ca5d1ac47860a24f277e4c94cc9cf097cb3672e33df078280214eb@group.calendar.google.com',
+    body=event).execute()
