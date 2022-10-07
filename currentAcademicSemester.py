9aa7b1a59cb3e1e1174167f66ee9dc55f7c44be4
Stanisław Kumor <stanislav.kumor@gmail.com>
2022-10-07 21:47:38 +0200

    bugfix

--- currentAcademicSemester.py
+++ currentAcademicSemester.py
@@ -3,7 +3,7 @@ import datetime
 current_time = datetime.datetime.now()
 month = current_time.month
 
-if month >= 8 or month ==1:
+if month >= 8 or month == 1:
     current_semester = "UEP Schedule " + str(current_time.year) + "/" + str(current_time.year + 1) + " Winter term"
 else:
     current_semester = "UEP Schedule " + str(current_time.year - 1) + "/" + str(current_time.year) + " Summer term"
