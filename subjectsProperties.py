2da42d8a8a9cb8d1fe4dae4281a40515f134efc7
Stanisław Kumor <stanislav.kumor@gmail.com>
2022-10-12 19:24:12 +0200

    create subject_properties_list

--- subjectsProperties.py
+++ subjectsProperties.py
@@ -15,11 +15,11 @@ def read_pdf_table(file):
     raw_thursday_schedule_list = data['Czwartek'].tolist()
     raw_friday_schedule_list = data['Piątek'].tolist()
 
-    week_schedule_list = [#raw_monday_schedule_list,
+    week_schedule_list = [raw_monday_schedule_list,
                           raw_tuesday_schedule_list,
-                          #raw_wednesday_schedule_list,
-                          #raw_thursday_schedule_list,
-                          #raw_friday_schedule_list,
+                          raw_wednesday_schedule_list,
+                          raw_thursday_schedule_list,
+                          raw_friday_schedule_list,
                           ]
 
     return week_schedule_list
@@ -32,7 +32,6 @@ subject_list_tat = [['nazwa przedmiotu 1', 'CEUE 1', '2022-10-08T12:00:00', '202
 def create_raw_subject_list(schedule_pdf):
     week_schedule_list = read_pdf_table(schedule_pdf)
     subject_list = []
-    subject_properties = []
 
     for day in week_schedule_list:
         for i in day:
@@ -41,54 +40,37 @@ def create_raw_subject_list(schedule_pdf):
                 subject_location = ''
                 subject_start = ''
                 subject_end = ''
-                print(i)
                 if i[2] == ':' and i[5] == '-' and i[8] == ':':  # zabezpieczyć jeśli jest mniej liter w stringu
                     subject_date = i.split("-")
                     if (week_schedule_list.index(day)) == 0:
                         subject_start = 'mondayT' + subject_date[0] + ':00'
                         subject_end = 'mondayT' + subject_date[1] + ':00'
-                        # print(subject_start)
-                        # print(subject_end)
                     elif (week_schedule_list.index(day)) == 1:
                         subject_start = 'tuesdayT' + subject_date[0] + ':00'
                         subject_end = 'tuesdayT' + subject_date[1] + ':00'
-                        # print(subject_start)
-                        # print(subject_end)
                     elif (week_schedule_list.index(day)) == 2:
                         subject_start = 'wednesdayT' + subject_date[0] + ':00'
                         subject_end = 'wednesdayT' + subject_date[1] + ':00'
-                        # print(subject_start)
-                        # print(subject_end)
                     elif (week_schedule_list.index(day)) == 3:
                         subject_start = 'thursdayT' + subject_date[0] + ':00'
                         subject_end = 'thursdayT' + subject_date[1] + ':00'
-                        # print(subject_start)
-                        # print(subject_end)
                     elif (week_schedule_list.index(day)) == 4:
                         subject_start = 'fridayT' + subject_date[0] + ':00'
                         subject_end = 'fridayT' + subject_date[1] + ':00'
-                        # print(subject_start)
-                        # print(subject_end)
-                    ##############################################
-                    next_subject = day[day.index(i) + 4]
-                    # print('---------------' +next_subject)
+                    next_property = day[day.index(i) + 1]
 
-                    if next_subject[2] == ':' and next_subject[5] == '-' and next_subject[8] == ':':
+                    if "Lab" in next_property or "Ćwi" in next_property or "Wyk" in next_property:
                         subject_name = day[day.index(i) + 1]
                         subject_location = day[day.index(i) + 2]
-
-                        print(subject_name)
-                        print(subject_location)
-                        print('---')
-
-
+                    else:
+                        subject_name = day[day.index(i) + 1] + ' ' + day[day.index(i) + 2]
+                        subject_location = day[day.index(i) + 3]
 
                 if subject_name != '':
-                    subject_properties.append(subject_name)
-                    subject_properties.append(subject_location)
-                    subject_properties.append(subject_start)
-                    subject_properties.append(subject_end)
-                print(subject_properties)
+                    subject_properties = [subject_name, subject_location, subject_start, subject_end]
+                    # print(subject_properties)
 
-
-
+                    subject_list.append(subject_properties)
+                    # print(subject_list)
+    for i in subject_list:
+        print(i)
