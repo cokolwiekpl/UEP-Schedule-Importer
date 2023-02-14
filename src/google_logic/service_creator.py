d6eddb062eb8085b76a704d0033d43ad10f177f1
Stanisław Kumor <stanislav.kumor@gmail.com>
2023-02-14 15:09:36 +0100

    token save to file deleted

--- src/google_logic/service_creator.py
+++ src/google_logic/service_creator.py
@@ -23,8 +23,12 @@ def create_service():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists(token_path):
-        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+
+
+    # if os.path.exists(token_path):TODO: nie sprawdzamy tokena bo go nie ma
+    #     creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+
+
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -32,9 +36,14 @@ def create_service():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(token_path, 'w') as token:
-            token.write(creds.to_json())
+
+
+        # Save the credentials for the next run TODO:nie tworzymy tokena na przyszłość
+        # with open(token_path, 'w') as token:
+        #     token.write(creds.to_json())
+
+
+
     try:
         service = build('calendar', 'v3', credentials=creds)
         print('User authorized')
@@ -42,3 +51,5 @@ def create_service():
 
     except HttpError as error:
         print('An error occurred: %s' % error)
+
+
